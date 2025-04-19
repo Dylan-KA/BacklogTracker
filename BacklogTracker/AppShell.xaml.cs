1459eb6188af3c6e0850210138dc0e96a4fd7195
Dylan Archer <111679436+Dylan-KA@users.noreply.github.com>
2025-04-19 17:23:35 +1000

    Add project files.

---
+++ BacklogTracker/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BacklogTracker
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ BacklogTracker/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace BacklogTracker
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
